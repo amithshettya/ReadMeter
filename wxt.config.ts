600d7b691beec6a4b351fe10259fb3f6cc95f455
Amith Shetty <amithshettya@gmail.com>
2026-02-16 21:35:03 -0600

    Add github CI to publish this repo to chrome and firefox

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
   srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  modules: ['@wxt-dev/module-svelte', '@wxt-dev/auto-icons'],
   manifest: {
+    name: 'ReadMeter',
     permissions: ['storage', 'activeTab', 'scripting'],
     host_permissions: ['<all_urls>'],
   },
